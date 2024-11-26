dd67e2886ca92ad9430ff55927c1e1779d3fb244
lukasmaar <lukas.maar@iaik.tugraz.at>
2024-11-26 09:55:10 +0100

    add support for AMD

--- cacheutils.h
+++ cacheutils.h
@@ -40,43 +40,53 @@ void reset_cpu_pin(void)
     }
 }
 
-static size_t rdtsc(void);
-static inline size_t rdtsc_nofence(void)
-{
-  size_t a, d;
-  asm volatile ("rdtsc" : "=a" (a), "=d" (d));
-  a = (d<<32) | a;
-  return a;
-}
+#define INTEL
+// #define AMD
 
-static inline size_t rdtsc(void)
-{
-  size_t a, d;
-  asm volatile ("mfence");
-  asm volatile ("rdtsc" : "=a" (a), "=d" (d));
-  a = (d<<32) | a;
-  asm volatile ("mfence");
-  return a;
-}
+#define RDPRU ".byte 0x0f, 0x01, 0xfd"
+#define RDPRU_ECX_MPERF	0
+#define RDPRU_ECX_APERF	1
 
 static inline size_t rdtsc_begin(void)
 {
+#if defined(INTEL)
   size_t a, d;
   asm volatile ("mfence");
   asm volatile ("rdtsc" : "=a" (a), "=d" (d));
   a = (d<<32) | a;
   asm volatile ("lfence");
   return a;
+#elif defined(AMD)
+  unsigned long low_a, high_a;
+  asm volatile ("mfence");
+  asm volatile(RDPRU
+			     : "=a" (low_a), "=d" (high_a)
+			     : "c" (RDPRU_ECX_APERF));
+  unsigned long aval = ((low_a) | (high_a) << 32);
+  asm volatile ("lfence");
+  return aval;
+#endif
 }
 
 static inline size_t rdtsc_end(void)
 {
+#if defined(INTEL)
   size_t a, d;
   asm volatile ("lfence");
   asm volatile ("rdtsc" : "=a" (a), "=d" (d));
   a = (d<<32) | a;
   asm volatile ("mfence");
   return a;
+#elif defined(AMD)
+  unsigned long low_a, high_a;
+  asm volatile ("lfence");
+  asm volatile(RDPRU
+			     : "=a" (low_a), "=d" (high_a)
+			     : "c" (RDPRU_ECX_APERF));
+  unsigned long aval = ((low_a) | (high_a) << 32);
+  asm volatile ("mfence");
+  return aval;
+#endif
 }
 
 static inline void flush(__attribute__((unused))size_t p)
